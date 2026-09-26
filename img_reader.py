6ae9f52eb05169f192b0e9fe8768cccf63c2c4f5
Mani Badri <moneybadri@gmail.com>
2026-09-25 22:20:48 -0400

    read boot sector added

--- img_reader.py
+++ img_reader.py
@@ -1,4 +1,9 @@
-
+#Color codes
+CRED = '\033[31m'
+CGREEN = '\033[32m'
+CYELLOW = '\033[33m'
+CBLUE = '\033[34m'
+CEND = '\033[0m'
 
 def read_MBR():
     with open("test.img", "rb") as image:
@@ -33,3 +38,18 @@ def read_bytes():
             data = bytearray(data)
             my_bytes = int(my_bytes)
             print(data[int(my_bytes*2):int(my_bytes*2)+2].decode("utf-8", errors="ignore")) 
+
+
+def read_boot_sector():
+    with open("test.img", "rb") as image:
+        data = image.read()
+    data = bytearray(data)
+    print(data[int(446*2):int(446*2)+2].decode("utf-8", errors="ignore"))
+    print(data[int(446*2):int(446*2)+2])
+    
+    print(CBLUE + "-----------------------------PARTITION 1 INFO-----------------------------" + CEND)
+    
+    if data[int(446*2):int(446*2)+2] == b"80":
+        print(CGREEN + "Partition is bootable." + CEND)
+    else:
+        print(CRED + "Partition is not bootable." + CEND)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ while True:
     if action == "write":
         img_maker.create_image()
     elif action == "read":
+        img_reader.read_boot_sector()
         action = input("Enter read action [(1)MBR] [(2)sector] [(3)hex] [(4)back]: ")
         if action == "1":
             img_reader.read_MBR()
